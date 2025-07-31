90eae4fc05d368f1c13407b3c775d64a321b39f3
neyluu <75064225+neyluu@users.noreply.github.com>
2025-07-31 13:57:02 +0200

    Init

---
+++ src/Engine/Engine.h
@@ -0,0 +1,18 @@
+#ifndef INC_2D_ENGINE_ENGINE_H
+#define INC_2D_ENGINE_ENGINE_H
+
+
+class Engine
+{
+public:
+    Engine();
+
+    void run();
+
+private:
+
+
+};
+
+
+#endif //INC_2D_ENGINE_ENGINE_H
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(1280, 720, "Hello world");
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

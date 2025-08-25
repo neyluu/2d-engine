6d9d91860e0447d4c60fc5ad215acf2e73205278
neyluu <75064225+neyluu@users.noreply.github.com>
2025-08-25 16:27:18 +0200

    FlappyBird cleanup

--- src/Games/FlappyBird/FlappyBird.cpp
+++ src/Games/FlappyBird/FlappyBird.cpp
@@ -4,8 +4,6 @@
 #include "Engine/Core.h"
 #include "Engine/Entities.h"
 
-using namespace e2d;
-
 enum SceneType
 {
     MAIN_MENU,
@@ -16,224 +14,91 @@ enum SceneType
 class Player : public e2d::GameObject
 {
 public:
-    Player(Scene* scene)
+    Player()
     {
-        m_owner = scene;
-        m_owner->addObject(&m_sprite);
+        e2d::SceneManager<SceneType>::get().getCurrentScene()->addObject(&m_sprite);
+        e2d::SceneManager<SceneType>::get().getCurrentScene()->addObject(&m_collider);
 
+        m_sprite.setSize(100, 100);
+        m_collider.setKinematic(true);
+        m_collider.setSize(m_sprite.getWidth(), m_sprite.getHeight());
 
-        m_sprite.transform.scale.x = 0.1;
-        m_sprite.transform.scale.y = 0.1;
-
-        m_sprite.transform.position.x = m_positionX;
-        m_sprite.transform.position.y = 100;
-
-        m_sprite.transform.rotation = 33;
-
-//        m_sprite.setScale(0.1);
-//        m_sprite.setScale(0.1, 1);
-//        m_sprite.setX(m_positionX);
-
+        updatePositions();
     }
 
     void update() override
     {
         float dt = GetFrameTime();
 
-//        if(IsKeyDown(KEY_SPACE)) velocity *= -1;
-//
-//        m_positionY += (100 * velocity) * dt;
-//
-//        m_sprite.setPosition(m_positionX, m_positionY);
+        float dy = 0;
+
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            velocityY =  -jumpForce;
+        }
+
+        velocityY += gravity * dt;
+
+        if(velocityY > maxFallingSpeed)
+        {
+            velocityY = maxFallingSpeed;
+        }
+
+        y += velocityY * dt;
+        updatePositions();
+    }
+
+    void updatePositions()
+    {
+        m_sprite.transform.position.x = x;
+        m_sprite.transform.position.y = y;
+
+        m_collider.transform.position.x = m_sprite.transform.position.x;
+        m_collider.transform.position.y = m_sprite.transform.position.y;
     }
 
 private:
-    Sprite m_sprite {"Assets/bird.png" };
+    e2d::Sprite m_sprite {"Assets/bird.png" };
+    e2d::BoxCollider m_collider { 100, 100 };
 
-    int m_positionX = -400;
-    int m_positionY = 0;
-
-    float velocity = 1;
+    int x = -400;
+    int y = 100;
+    float velocityY = 10;
+    float gravity = 2000;
+    float jumpForce = 575;
+    float maxFallingSpeed = 300;
 };
 
 
-class Game : GameObject
+class Game : public e2d::GameObject
 {
 public:
     Game()
     {
         SetWindowTitle("Flappy Bird");
-        SceneManager<SceneType>::get().addScene(SceneType::GAME, &m_gameScene);
-        SceneManager<SceneType>::get().setScene(SceneType::GAME);
-        m_gameScene.addObject(this);
-        m_gameScene.addObject(&m_player);
-
-        m_gameScene.addObject(&mov1);
-        m_gameScene.addObject(&mov2);
-//
-        m_gameScene.addObject(&col2);
-        m_gameScene.addObject(&col3);
-//        m_gameScene.addObject(&col4);
-//
-        mov1.setKinematic(true);
-        mov2.setKinematic(true);
-        col4.setKinematic(true);
-//
-        mov1.transform.position.x = -200;
-        mov1.transform.position.y = -200;
-
-        mov2.transform.position.x = 500;
-        mov2.transform.position.y = 300;
-
-        col2.transform.position.x = 0;
-        col2.transform.position.y = 0;
-        col2.transform.scale.x = 1.5;
-
-        col3.transform.position.x = 125;
-        col3.transform.position.y = 125;
-        col3.transform.scale.y = 0.5;
-
-        mov1.setOnCollideEnter([]{std::cout << "Enter\n";});
-        mov1.setOnCollide([]{std::cout << "Collide!\n";});
-        mov1.setOnCollideExit([]{std::cout << "Exit!\n";});
-//
-//        mov2.setOnCollideEnter([]{std::cout << "Enter\n";});
-//        mov2.setOnCollide([]{std::cout << "Collide!\n";});
-//        mov2.setOnCollideExit([]{std::cout << "Exit!\n";});
-//
-//        mov1.setOnCollideEnter([]{std::cout << "Enter222\n";}, col4);
-//        mov1.setOnCollide([]{std::cout << "Collide!222\n";}, col4);
-//        mov1.setOnCollideExit([]{std::cout << "Exit!222\n";}, col4);
-//
-//
-//        col3.transform.position.x = 350;
-//        col3.transform.position.y = 200;
-//
-//
-//
-//
-//
-//
-//
-        mov1.m_isVisible = true;
-        mov2.m_isVisible = true;
-        col2.m_isVisible = true;
-        col3.m_isVisible = true;
-        col4.m_isVisible = true;
+        e2d::SceneManager<SceneType>::get().getCurrentScene()->addObject(&m_player);
     }
 
     void update() override
     {
-        handleMovingMov1();
-        handleMovingMov2();
-//        col2.transform.position.x -= 1000 * GetFrameTime();
+
     }
 
-    void handleMovingMov1()
-    {
-//        mov1.transform.scale.x += 0.01;
-
-        int dx = 0;
-        int dy = 0;
-        float dt = GetFrameTime();
-
-//        Rectangle rect = mov1.getBox();
-
-        int x = mov1.transform.position.x;
-        int y = mov1.transform.position.y;
-
-
-        float change = speed * dt;
-
-        if(IsKeyDown(KEY_W))
-        {
-            dy -= change;
-        }
-        if(IsKeyDown(KEY_D))
-        {
-            dx += change;
-        }
-        if(IsKeyDown(KEY_S))
-        {
-            dy += change;
-        }
-        if(IsKeyDown(KEY_A))
-        {
-            dx -= change;
-        }
-
-        x += dx;
-        y += dy;
-//        std::cout << dx << " " << dy << "\n";
-
-        mov1.transform.position.x = x;
-        mov1.transform.position.y = y;
-    }
-
-    void handleMovingMov2()
-    {
-        int dx = 0;
-        int dy = 0;
-        float dt = GetFrameTime();
-
-        int x = mov2.transform.position.x;
-        int y = mov2.transform.position.y;
-
-
-        float change = speed * dt;
-
-        if(IsKeyDown(KEY_UP))
-        {
-            dy -= change;
-        }
-        if(IsKeyDown(KEY_RIGHT))
-        {
-            dx += change;
-        }
-        if(IsKeyDown(KEY_DOWN))
-        {
-            dy += change;
-        }
-        if(IsKeyDown(KEY_LEFT))
-        {
-            dx -= change;
-        }
-
-        x += dx;
-        y += dy;
-//        std::cout << dx << " " << dy << "\n";
-
-        mov2.transform.position.x = x;
-        mov2.transform.position.y = y;
-    }
 private:
-//    int colX = -200;
-//    int colY = 80;
-    float speed = 500;
-
-    Scene m_gameScene {};
-    Player m_player { &m_gameScene };
-
-//    BoxCollider col { 100, 100 };
-
-    BoxCollider mov1 { 100, 100 };
-    BoxCollider mov2 { 100, 100 };
-//
-    BoxCollider col2 { 150, 150 };
-    BoxCollider col3 { 100, 100 };
-//
-    BoxCollider col4 { 420, 69 };
-
-
+    Player m_player { };
 };
 
 
 int main()
 {
-    SceneManager<SceneType>& sceneManager = SceneManager<SceneType>::get();
+    e2d::SceneManager<SceneType>& sceneManager = e2d::SceneManager<SceneType>::get();
 
-    Engine engine { sceneManager };
+    e2d::Engine engine { sceneManager };
     engine.init();
+
+    e2d::Scene m_gameScene {};
+    e2d::SceneManager<SceneType>::get().addScene(SceneType::GAME, &m_gameScene);
+    e2d::SceneManager<SceneType>::get().setScene(SceneType::GAME);
 
     Game game {};
 
